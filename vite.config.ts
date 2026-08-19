c1855361467a94b907375cdc0419ed016c02fcbc
Roman K <klimusr@gmail.com>
2026-08-19 20:49:42 +0300

    Fix Cloudflare deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { resolve } from "path";
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), cloudflare()],
   build: {
     rollupOptions: {
       input: {
